ebd426422fb2c19ce4c3efdc0c3baa5593d0fae9
danielvu1994 <vulinhdam1994@gmail.com>
2021-04-18 21:10:10 +0700

    Add verify inputs + test case

---
+++ lib/pokerhands.ex
@@ -0,0 +1,71 @@
+defmodule Pokerhands do
+  @moduledoc """
+  Documentation for `Pokerhands`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pokerhands.hello()
+      :world
+
+  """
+  def cal_result(black, white) do
+    {valid, reason} = is_card_valid?(black ,white)
+
+    if valid == :error do
+      {valid, reason}
+    else    # :ok
+      compare(black, white)
+    end
+  end
+
+  defp compare(_black, _white) do
+    :true
+  end
+
+
+  # Compare part
+  defp is_card_valid?(black, white) do
+    {valid_b, reason_b} = player_cards_valid?(black)
+    {valid_w, reason_w} = player_cards_valid?(white)
+
+    case {valid_b, valid_w} do
+      {:ok, :ok} ->
+        {:ok, "All are good"}
+      {:error, :error} ->
+        {:error, "Black: #{reason_b}, White: #{reason_w}"}
+      {:error, :ok} ->
+        {:error, "Black: #{reason_b}"}
+      {:ok, :error} ->
+        {:error, "White: #{reason_w}"}
+    end
+  end
+
+  defp player_cards_valid?(cards) do
+    cards=
+      cards
+      |> String.split([" "], trim: true)
+      |> Enum.map(&String.upcase(&1))
+
+    num_of_cards = length(cards)
+
+    valid_cards =
+      cards
+      |> Enum.filter(&String.match?(&1,  ~r{^[0-9TJQKA][HDCS]}) == true)
+      # |> IO.inspect
+      |> length
+
+    player_cards_valid?(num_of_cards, valid_cards)
+  end
+
+  defp player_cards_valid?(5, 5), do: {:ok, "Cards are good"}
+  defp player_cards_valid?(num, valid) when num < 5 and valid < num,
+  do: {:error, "Cards are not enough! - Invalid cards!!"}
+  defp player_cards_valid?(num, _valid) when num < 5,
+  do: {:error, "Cards are not enough!"}
+  defp player_cards_valid?(5, valid) when valid < 5,
+  do: {:error, "Invalid cards!!"}
+end
---
+++ test/pokerhands_test.exs
@@ -0,0 +1,51 @@
+defmodule PokerhandsTest do
+  use ExUnit.Case
+  # doctest Pokerhands
+
+  test "Lack of a card" do
+    assert {:error, "Black: Cards are not enough!"}  =
+      Pokerhands.cal_result("2H 3D 5S 9C", "2C 3H 4S 8C AH")
+  end
+
+  # @tag :pending
+  test "Invalid input" do
+    assert {:error, "White: Invalid cards!!"}  =
+      Pokerhands.cal_result("2H 3D 5S 9C KD", "2C 3H 4S 8C Ap")
+  end
+
+  # @tag :pending
+  test "Both players's inputs are wrong" do
+    assert {:error, "Black: Cards are not enough!, White: Invalid cards!!"}  =
+      Pokerhands.cal_result("2H 3D 5S 9C", "2C 3H 4S 8C Ap")
+  end
+
+  # @tag :pending
+  test "Complicated error" do
+    assert {:error, "Black: Cards are not enough! - Invalid cards!!, White: Cards are not enough! - Invalid cards!!"}  =
+      Pokerhands.cal_result("2H 3D 5S 9p", "2C 3H 8C Ap")
+  end
+
+  @tag :pending
+  test "White win with High Card" do
+    assert Pokerhands.cal_result("2H 3D 5S 9C KD", "2C 3H 4S 8C AH") ==
+      {:ok, "White wins. - with high card: Ace"}
+  end
+
+  @tag :pending
+  test "Black  hite win with full house" do
+    assert Pokerhands.cal_result("2H 4S 4C 2D 4H", "2S 8S AS QS 3S") ==
+      {:ok, "Black wins. - with full house: 4 over 2"}
+  end
+
+  @tag :pending
+  test "Black win with high card" do
+    assert Pokerhands.cal_result("2H 3D 5S 9C KD", "2C 3H 4S 8C KH") ==
+      {:ok, "Black wins. - with high card: 9"}
+  end
+
+  @tag :pending
+  test "Tie." do
+    assert Pokerhands.cal_result("2H 3D 5S 9C KD", "2D 3H 5C 9S KH") ==
+      {:ok, "Tie."}
+  end
+end
